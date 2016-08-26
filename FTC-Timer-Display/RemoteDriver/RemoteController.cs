8aa6ab6d20f29c6e58fe615a160a0d6ac98c2863
mglennon <mglennon@nsandd.com>
2016-08-26 10:19:43 -0400

    Start progress towards physical remote button.

---
+++ FTC-Timer-Display/RemoteDriver/RemoteController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTC_Timer_Display.RemoteDriver
+{
+    public static class RemoteController
+    {
+
+    }
+}
