b28986e589643bae8d4741a14b5ba4b08eefedae
unknown <mglennon@VIRTDESK.homeoffice.nsandd.com>
2016-10-25 09:51:05 -0400

    Updated wizard VAF logo.

--- FTC-Timer-Display/frmInitialSetup.Designer.cs
+++ FTC-Timer-Display/frmInitialSetup.Designer.cs
@@ -192,7 +192,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Image = global::FTC_Timer_Display.Properties.Resources.vaflogo;
+            this.pictureBox1.Image = global::FTC_Timer_Display.Properties.Resources.vaflogo_new;
             this.pictureBox1.Location = new System.Drawing.Point(3, 3);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(144, 70);
