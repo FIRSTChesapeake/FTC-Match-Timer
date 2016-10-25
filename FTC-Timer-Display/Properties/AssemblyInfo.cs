b2cb3dce7e478252f2dd78aee00531fcac8d0df0
unknown <mglennon@VIRTDESK.homeoffice.nsandd.com>
2016-10-25 10:01:55 -0400

    Version Info Update

--- FTC-Timer-Display/Properties/AssemblyInfo.cs
+++ FTC-Timer-Display/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("16.1609.14.*")]
-[assembly: AssemblyFileVersion("16.1609.14.0")]
+[assembly: AssemblyVersion("16.1610.25.*")]
+[assembly: AssemblyFileVersion("16.1610.25.0")]
